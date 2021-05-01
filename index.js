7a76d5fbf2af6ded1e1df97613168b0fc898778c
Kshitij Baluni <kshitij.baluni@gmail.com>
2021-05-01 15:21:16 +0000

    Updated and restructured the function.

--- index.js
+++ index.js
@@ -5,31 +5,6 @@
 * @since 01 May, 2021
 **/
 
-//O(a + b) Time complexity
-//O(1) Space complexity
-function useTwoInputs(a, b) {
-  console.log("Two Inputs function -> ");
-  for(let i = 0;i < a.length; i++) {
-    console.log(a[i]);
-  }
-
-  for(let i = 0;i < b.length; i++) {
-    console.log(b[i]);
-  }
-}
-
-//Updated for loop to for each
-function useTwoInputsV2(a, b) {
-  console.log("Two Inputs function Version 2 -> ");
-  a.forEach({ data =>
-    console.log(data)
-  })
-
-  b.forEach({data =>
-    console.log(data)
-  })
-}
-
 //O(n) Time complexity
 //O(1) Space complexity
 function useOneInputs(n) {
@@ -47,7 +22,34 @@ function useOneInputsV2(n) {
   })
 }
 
+//O(a + b) Time complexity
+//O(1) Space complexity
+function useTwoInputs(a, b) {
+  console.log("Two Inputs function -> ");
+  for(let i = 0;i < a.length; i++) {
+    console.log(a[i]);
+  }
+
+  for(let i = 0;i < b.length; i++) {
+    console.log(b[i]);
+  }
+}
+
+//Updated for loop to for each
+function useTwoInputsV2(a, b) {
+  console.log("Two Inputs function Version 2 -> ");
+  a.forEach( data => {
+    console.log(data)
+  })
+
+  b.forEach(data =>{
+    console.log(data)
+  })
+}
+
+//Test Data for the above functions
 useOneInputs([1,2,3,4,6,4,7,3,6])
 useTwoInputs([1,2,3,4,6,4,7,3,6], [23,42,52,])
 
 useOneInputsV2([1,2,3,4,6,4,7,3,6])
+useTwoInputsV2([1,2,3,4,6,4,7,3,6], [23,42,52,])
