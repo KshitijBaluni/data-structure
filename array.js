9ead02c18957736a9a37ad69ab6eb6e5742f0615
Kshitij Baluni <kshitij.baluni@gmail.com>
2021-05-06 07:34:26 +0000

    added functionality for array.

--- array.js
+++ array.js
@@ -1,3 +1,9 @@
+/**
+* Data Structure : Array
+* 
+* @author Kshitij Baluni
+* @since 06 May, 2021
+**/
 class Array {
   constructor() {
     this.length = 0;
@@ -7,7 +13,39 @@ class Array {
   get(index) {
     return this.data[index];
   }
+
+  push(value) {
+    this.data[this.length] = value;
+    this.length++;
+  }
+
+  pop() {
+    const lastValue = this.data[this.length-1];
+    delete this.data[this.length -1];
+    this.length--;
+    return lastValue;
+  }
+
+  delete(index) {
+    const valueDelete = this.data[index];
+    this.shift(index);
+    delete this.data[this.length -1];
+    return valueDelete;
+  }
+
+  shift(index) {
+    for(let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+  }
 }
 
 const myArray = new Array();
-console.log(myArray.get(0));
+myArray.push("hi");
+myArray.push("how");
+myArray.push("are");
+myArray.push("you");
+myArray.push("?");
+myArray.pop();
+myArray.delete(3);
+console.log(myArray);
