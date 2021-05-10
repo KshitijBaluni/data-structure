78dfe24c6a3cd0f3d9b5a2e21de06e255bfeb3cc
Kshitij Baluni <kshitij.baluni@gmail.com>
2021-05-10 17:09:15 +0000

    added docs

--- reverse_string.js
+++ reverse_string.js
@@ -1,5 +1,19 @@
-
+/**
+* Data Structure : Array
+* 
+* @author Kshitij Baluni
+* @since 08 May, 2021
+**/
 
 function reverseString(str) {
-
+  console.log("String reversed:: ");
+  let revStr = [];
+  let j=0;
+  for(let i = str.length -1; i >= 0; i--) {
+    revStr[j++] = str[i];
+  }
+  console.log(revStr.join(''));
 }
+
+//Test data
+reverseString("string");
